373bc9a6f0ee46c7578b6759709ff963463165c2
Iheb Ibenlouafi <belwafiiheb1@gmail.com>
2024-11-06 22:54:05 +0100

    Test

---
+++ src/test/java/Test.java
@@ -0,0 +1,12 @@
+package tn.esprit.esprithub;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Test {
+    @Test
+    void dummyTest() {
+        // This test always passes
+        assertTrue(true);
+    }
+}
